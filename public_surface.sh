b37457b9a322eb6259e1eff56b60e66fc3a1a259
Default Application User <ecp@mgl.ca>
2026-01-31 23:56:22 +0100

    add pub9ic copy script

---
+++ public_surface.sh
@@ -0,0 +1,18 @@
+# Copy sanitized Files to public surface repo
+#Chip_top.v needs sanitization
+cp public_surface.sh ../CrossLink_LCD/.
+cp chip_top.v  ../CrossLink_LCD/.
+cp commit.mem ../CrossLink_LCD/.
+cp format_commit.awk ../CrossLink_LCD/.
+cp README.md  ../CrossLink_LCD/.
+cp run_test.sh  ../CrossLink_LCD/.
+cp testbench.sv  ../CrossLink_LCD/.
+cp testgen1.sty	../CrossLink_LCD/.
+cp testgen.ccl  ../CrossLink_LCD/.
+cp testgen.ldc  ../CrossLink_LCD/.
+cp testgen.ldf  ../CrossLink_LCD/.
+cp testgen.lpf  ../CrossLink_LCD/.
+cp watermark.sh ../CrossLink_LCD/.
+cp impl1/testgen_impl1_lattice.synproj  ../CrossLink_LCD/impl1/.
+cp impl1/testgen_impl1.lpf ../CrossLink_LCD/impl1/.
+cp mipi_dsi_tx/mipi_dsi_tx.v ../CrossLink_LCD/mipi_dsi_tx/.
