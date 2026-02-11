7362abeb5eb20f7ab0dd280d56f5b835651124c6
Default Application User <ecp@mgl.ca>
2026-02-11 17:01:47 +0100

    remove temp file from repo

--- public_surface.sh
+++ public_surface.sh
@@ -13,7 +13,5 @@ cp testgen.ldc  ../CrossLink_LCD/.
 cp testgen.ldf  ../CrossLink_LCD/.
 cp testgen.lpf  ../CrossLink_LCD/.
 cp watermark.sh ../CrossLink_LCD/.
-cp impl1/testgen_impl1_lattice.synproj  ../CrossLink_LCD/impl1/.
-cp impl1/testgen_impl1.lpf ../CrossLink_LCD/impl1/.
 cp mipi_dsi_tx/mipi_dsi_tx.v ../CrossLink_LCD/mipi_dsi_tx/.
 cp BoardTestList.txt ../CrossLink_LCD/.
